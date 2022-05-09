1e7f66d379a3a561eb3a5bd2402a1232a406d259
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-05-09 17:04:22 -0400

    Create Surface Area and Volume of a Box.js

---
+++ Surface Area and Volume of a Box.js
@@ -0,0 +1,7 @@
+Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(width, height, depth){
+  let area = (2*width*height)+(2*height*depth)+(2*depth*width)
+  let volume = width * height * depth
+  return [area, volume]
+}
