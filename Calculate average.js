b01285b2598ec6b17fd04c6200369c0da115f13c
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-06-03 11:58:15 -0400

    Update Calculate average.js

--- Calculate average.js
+++ Calculate average.js
@@ -3,5 +3,5 @@ Write a function which calculates the average of the numbers in a given list.
 Note: Empty arrays should return 0.
 
 function find_average(array){
-  return array.reduce((a,b)=> a+b,0)/array.length;
+return array.length == 0 ? 0 : array.reduce((a,b)=>a+b)/array.length;
 }
