d9e9a7600afb2ed2f5a75e4c0fa3d59e517d5511
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-07-06 10:16:48 -0400

    Create  Multiplication table.js

---
+++ Multiplication table.js
@@ -0,0 +1,18 @@
+Your task, is to create NxN multiplication table, of size provided in parameter.
+
+for example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+
+function multiplicationTable(size){
+  let table = [];
+  for (i = 1; i <= size; i++){
+    let row = [];
+    for (j = 1; j <= size; j++) row.push(i * j);
+      table.push(row);
+    }
+  return table;
+}
