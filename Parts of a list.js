784db4193d23461b7cc4de61fdfda9cf2d140e5c
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-07-09 22:19:57 -0400

    Create Parts of a list.js

---
+++ Parts of a list.js
@@ -0,0 +1,7 @@
+Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+Each part will be in a string
+Elements of a pair must be in the same order as in the original array.
+
+partlist=(arr)=>arr.map((v, i) => [arr.slice(0, i).join(' '), arr.slice(i).join(' ')]).slice(1)
