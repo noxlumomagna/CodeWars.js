762fd46d0ccafb4a62b89a016e9e8e86113059ad
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-07-12 14:33:08 -0400

    Create Buying a car.js

---
+++ Buying a car.js
@@ -0,0 +1,24 @@
+Let us begin with an example:
+
+A man has a rather old car being worth $2000. He saw a secondhand car being worth $8000. He wants to keep his old car until he can buy the secondhand one.
+
+He thinks he can save $1000 each month but the prices of his old car and of the new one decrease of 1.5 percent per month. Furthermore this percent of loss increases of 0.5 percent at the end of every two months. Our man finds it difficult to make all these calculations.
+
+Can you help him?
+
+How many months will it take him to save up enough money to buy the car he wants, and how much money will he have left over?
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+  let months = 0;
+  let savings = startPriceOld
+    for(let i=1; savings<startPriceNew;i++){
+      if(i%2===0){
+        percentLossByMonth+=0.5
+      }
+      months++;
+      startPriceOld = startPriceOld - (startPriceOld*(percentLossByMonth/100));
+      startPriceNew = startPriceNew - ((startPriceNew*percentLossByMonth)/100);
+      savings = startPriceOld + (savingperMonth*months);
+    }
+  return [months,Math.round(savings-startPriceNew)];
+}
