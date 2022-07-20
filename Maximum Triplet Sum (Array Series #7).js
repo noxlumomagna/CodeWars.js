c4ff1f186c7738c69dffc00da336533254c03ddf
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-07-19 20:05:45 -0400

    Create Maximum Triplet Sum (Array Series #7).js

---
+++ Maximum Triplet Sum (Array Series #7).js
@@ -0,0 +1,15 @@
+Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+Notes :
+Array/list size is at least 3 .
+
+Array/list numbers could be a mixture of positives , negatives and zeros .
+
+Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+
+
+function maxTriSum(numbers){
+  let sorted = [...new Set(numbers.sort((a,b)=>b-a))]
+  return sorted[0]+sorted[1]+sorted[2]                       
+}
