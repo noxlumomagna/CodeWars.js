ac5fcd3f81b6a2151a3b7a1f712c6429fd874b2b
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-07-24 00:26:29 -0400

    Update Grader.js

--- Grader.js
+++ Grader.js
@@ -8,9 +8,9 @@ Anything greater than 1 or less than 0.6	"F"
 0.6 or greater	"D"
 
 function grader(score) {
+  if(score > 1 || score < 0.6) return "F"
   if(score >= 0.9) return "A"
   if(score >= 0.8) return "B"
   if(score >= 0.7) return "C"
   if(score >= 0.6) return "D"
-  if(score > 1 || score < 0.6) return "F"
 }
