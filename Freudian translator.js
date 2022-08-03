52526a51b093c28ecb190816ab667678f59691f7
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-03 13:28:58 -0400

    Update Freudian translator.js

--- Freudian translator.js
+++ Freudian translator.js
@@ -7,13 +7,6 @@ Now, basically, when a person hears about Freud, s/he hears "sex" because for Fr
 In this kata, the toFreud() function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. Note that an empty string, or no arguments, should result in the ouput being ""(empty string).
 
 function toFreud(string) {
-  if(string === ""){
-    return string;
-  }
-  string = string.split(' ');
-  for(var i = 0; i < string.length; i++){
-    string[i] = 'sex';
-  }
-  string = string.join(' ');
-  return string;
+  if(!string) return ""
+   return string.split(' ').map(e=>e='sex').join(" ")
 }
