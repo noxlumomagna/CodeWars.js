578af787ed360823cb7c60c1f48cac675e05d61b
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-06 21:20:42 -0400

    Create  Calculate Price Excluding VAT.js

---
+++ Calculate Price Excluding VAT.js
@@ -0,0 +1,10 @@
+Write a function that calculates the original product price, without VAT.
+
+Example
+If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+
+Thus, if your function receives 230.00 as input, it should return 200.00
+
+function excludingVatPrice(price){
+  return price === null? -1 : +(price/1.15).toFixed(2)
+}
