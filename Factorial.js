27976ee32668bc8f9d736fc20b0ddb462dee8123
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-08 18:48:47 -0400

    Update Factorial.js

--- Factorial.js
+++ Factorial.js
@@ -3,7 +3,7 @@ Your task is to write function factorial.
 function factorial(n){
   if (n === 1 || n === 0){
     return 1;
-  } for (var i = n - 1; i >= 1; i--){
+  } for (let i=n-1; i>=1; i--){
     n *= i
   }
   return n
