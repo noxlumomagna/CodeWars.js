dae1ba694a599f733955d655694b0ec1da140dbd
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-10 07:28:42 -0400

    Create Classy Extentions.js

---
+++ Classy Extentions.js
@@ -0,0 +1,8 @@
+Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+The name attribute is passed with this.name (JS), @name (Ruby) or self.name (Python).
+
+class Cat extends Animal {
+  speak(){
+    return `${this.name} meows.`;
+    }
+  }
