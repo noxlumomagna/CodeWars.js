8343b353e6b9f46094d5067bafa563d6b7cc4b96
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-12 18:27:21 -0400

    Update Nth Smallest Element (Array Series #4).js

--- Nth Smallest Element (Array Series #4).js
+++ Nth Smallest Element (Array Series #4).js
@@ -1,8 +1,9 @@
 Given an array/list [] of integers , Find the Nth smallest element in this array of integers
 
 function nthSmallest(arr,pos){
-  let sorted = arr.sort((a,b)=>a-b)
-  for(let i=0; i<sorted.length;i++){
-    return sorted[pos-1]
-  }
+  return arr.sort((a,b)=>a-b)[pos-1]
 }
+
+or
+
+nthSmallest=(arr,pos)=>arr.sort((a,b)=>a-b)[pos-1]
