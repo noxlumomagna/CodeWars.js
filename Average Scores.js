f97da1c87c2593554cde69cc0d20ad54c2c71fcb
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-18 10:56:34 -0400

    Create  Average Scores.js

---
+++ Average Scores.js
@@ -0,0 +1,9 @@
+Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+
+function average(scores) {
+  return Math.round(scores.reduce((a,b)=>a+b,0)/scores.length)
+}
+
+or
+
+average=scores=>Math.round(scores.reduce((a,b)=>a+b,0)/scores.length)
