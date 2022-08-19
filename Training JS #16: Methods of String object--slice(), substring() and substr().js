fafac736462ccb300347fb1e0404160357619101
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-19 13:22:19 -0400

    Create Training JS #16: Methods of String object--slice(), substring() and substr().js

---
+++ Training JS #16: Methods of String object--slice(), substring() and substr().js
@@ -0,0 +1,10 @@
+Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+The first mission: Traversing arr, find the shortest string length.
+
+The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+function cutIt(arr){
+  let shortest = Math.min(...arr.map(str => str.length))
+  return arr.map(str => str.slice(0, shortest))
+}
