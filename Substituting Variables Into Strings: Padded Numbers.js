46bcce7dc3ec6fe32c8d05b0c7a1a8ce8f772c48
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-23 19:02:01 -0400

    Create Substituting Variables Into Strings: Padded Numbers.js

---
+++ Substituting Variables Into Strings: Padded Numbers.js
@@ -0,0 +1,5 @@
+Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+function solution(value){
+  return "Value is " + ("00000" + value).slice(-5);
+}
