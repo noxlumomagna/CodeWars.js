18404980394aeb869bfe93d75f304dc33b0a7a35
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-24 21:02:48 -0400

    Update Capitalize first letter of a string.js

--- Capitalize first letter of a string.js
+++ Capitalize first letter of a string.js
@@ -2,7 +2,7 @@ Write a function capitalize() which capitalizes the first letter (if any) of the
 
 String.prototype.capitalize = function () {
   let c = this.charCodeAt(0)
-  if (97 <= c && c <= 122) c -= 32
+  if (96 < c && c < 123) c -= 32
   
   return String.fromCharCode(c) + this.slice(1)
 }
