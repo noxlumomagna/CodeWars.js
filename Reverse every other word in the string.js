b206b75fd46a600c48c6b12db0c0fbdaa41c8e13
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-30 12:50:31 -0400

    Update Reverse every other word in the string.js

--- Reverse every other word in the string.js
+++ Reverse every other word in the string.js
@@ -1,6 +1,5 @@
 Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
 
 function reverse(str){
-  if(str.split(' ').map((e,i)=>i%2!==0 ? e.split('').reverse().join(''): e).join(' ') === '   ') return ''
-  return str.split(' ').map((e,i)=>i%2!==0 ? e.split('').reverse().join(''): e).join(' ')
+  return str.split(' ').map((e,i)=>i%2!==0 ? e.split('').reverse().join(''): e).join(' ').trim()
 }
