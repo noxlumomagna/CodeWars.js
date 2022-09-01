2b86a668f0f5c207349214ee612a8d241a2c7c32
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-09-01 14:32:41 -0400

    Create Find the lucky numbers.js

---
+++ Find the lucky numbers.js
@@ -0,0 +1,3 @@
+Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include the elements that contain the digit 7.
+
+filterLucky=x=>x.filter(e=>e.toString().includes(7))
