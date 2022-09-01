92e429ca8543605a65d1f10fb99e1c599b58068c
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-09-01 11:05:42 -0400

    Create Say hello!.js

---
+++ Say hello!.js
@@ -0,0 +1,3 @@
+Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+
+greet=name=>name?`hello ${name}!` : null
