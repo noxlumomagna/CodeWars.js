a0115daafd74424623c6de7c48301e8c5146cf73
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-09-01 12:15:32 -0400

    Create By 3, or not by 3? That is the question . . ..js

---
+++ By 3, or not by 3? That is the question . . ..js
@@ -0,0 +1,5 @@
+A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+
+Given a series of digits as a string, determine if the number represented by the string is divisible by three.
+
+divisibleByThree=str=>Number.isInteger(str.split('').reduce((a,b)=>a+b*1,0)/3)
