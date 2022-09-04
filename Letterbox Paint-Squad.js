ab3ded947ce923e570c8f674e2078b01911703d2
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-09-03 23:31:52 -0400

    Create Letterbox Paint-Squad.js

---
+++ Letterbox Paint-Squad.js
@@ -0,0 +1,14 @@
+Given the start and end letterbox numbers, write a method to return the frequency of all 10 digits painted.
+
+Example
+For start = 125, and end = 132
+
+function paintLetterboxes(start,end){
+  let boxes = Array(10).fill(0)
+  for (let i=start; i<=end; i++){
+for(let n of(i+'')){
+  boxes[n]++
+    }
+  }
+  return boxes
+}
