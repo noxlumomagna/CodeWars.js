a917bf5b4e17223d0775756bd726085c80e639dd
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-09-12 13:30:32 -0400

    Update Number Of Occurrences.js

--- Number Of Occurrences.js
+++ Number Of Occurrences.js
@@ -1,7 +1,5 @@
 Write a function that returns the number of occurrences of an element in an array.
 
-
-
 Array.prototype.numberOfOccurrences = function(number){
   return this.filter((e)=>e==number).length
 }
