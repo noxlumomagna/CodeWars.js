cc990dfdb38cad1bc7c930949dcf0cee726eb788
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-09-21 13:39:25 -0400

    Create Over The Road.js

---
+++ Over The Road.js
@@ -0,0 +1,5 @@
+Given your house number address and length of street n, give the house number on the opposite side of the street.
+
+function overTheRoad(address, n){
+  return (n * 2 + 1) - address
+}
