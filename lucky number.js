d645ddfb033f86450903fe9b4a8f08807f511a5d
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-09-23 12:58:05 -0400

    Update lucky number.js

--- lucky number.js
+++ lucky number.js
@@ -3,3 +3,14 @@ Write a function to find if a number is lucky or not. If the sum of all digits i
 function isLucky(n) {
   return String(n).split('').reduce((a,b)=>a+b,0) % 9 === 0
 }
+
+
+or
+
+function isLucky(n){
+  return n % 9 === 0
+}
+
+or
+
+isLucky=n=>n%9===0
