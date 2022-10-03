b45101b70eeedb8bf959bcbbafdb9e947814e466
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-10-02 20:31:18 -0400

    Create The Poet And The Pendulum.js

---
+++ The Poet And The Pendulum.js
@@ -0,0 +1,13 @@
+Given an array/list [] of n integers , Arrange them in a way similar to the to-and-fro movement of a Pendulum
+
+The Smallest element of the list of integers , must come in center position of array/list.
+
+The Higher than smallest , goes to the right .
+The Next higher number goes to the left of minimum number and So on , in a to-and-fro manner similar to that of a Pendulum.
+
+function pendulum(values) {
+  let leftPointer = []
+  let rightPointer = []
+  let sorted = values.sort((a,b)=>a-b).map((e,i)=>(i%2===0? leftPointer : rightPointer).push(e))
+  return leftPointer.reverse().concat(rightPointer)
+}
