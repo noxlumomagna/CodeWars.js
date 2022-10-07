044474c090f4240ac2c66a14b314110d4fd98fe2
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-10-07 15:30:09 -0400

    Update Linked Lists - Length & Count.js

--- Linked Lists - Length & Count.js
+++ Linked Lists - Length & Count.js
@@ -32,3 +32,19 @@ function count(head, data) {
   }
   return count;
 }
+
+or
+
+function Node(data) {
+  this.data = data
+  this.next = null
+}
+
+function length(head) {
+  return head ? 1 + length(head.next) : 0
+}
+
+function count(head, data) {
+  if (!head) return 0
+  return (head.data === data ? 1 : 0) + count(head.next, data)
+}
