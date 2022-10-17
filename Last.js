44da2668cf4e9a7a40dec5ebd782a80417ad04c5
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-10-17 17:28:00 -0400

    Update Last.js

--- Last.js
+++ Last.js
@@ -2,5 +2,5 @@ Find the last element of the given argument(s).
 
 function last(list){
   let last = arguments[arguments.length-1]
-  return list[list.length-1] || last
+  return last[last.length-1] || last
 }
