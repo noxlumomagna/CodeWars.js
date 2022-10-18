b024ac51c59ea3f37e6145d32c52284144a45108
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-10-17 20:26:36 -0400

    Update Unique Sum.js

--- Unique Sum.js
+++ Unique Sum.js
@@ -1,6 +1,5 @@
 Given a list of integers values, your job is to return the sum of the values; however, if the same integer value appears multiple times in the list, you can only count it once in your sum.
 
-function uniqueSum(array){
-  let set = new Set(array)
- return array.length ? [...set].reduce((a,b)=>a+b,0) : null
+function uniqueSum(arr){
+  return [...new Set(arr)].reduce((a,b)=>a+b,null)
 }
