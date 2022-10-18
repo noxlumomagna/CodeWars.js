9ab645fcaa37250fc56d1fc6a9f4ca35111e6d2e
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-10-17 20:36:25 -0400

    Create Frequency sequence.js

---
+++ Frequency sequence.js
@@ -0,0 +1,7 @@
+Return an output string that translates an input string s/$s by replacing each character in s/$s with a number representing the number of times that character occurs in s/$s and separating each number with the character(s) sep/$sep.
+
+function freqSeq(str, sep) {
+  let object = {}
+  str.split('').map(e=>object[e] = object[e]? object[e]+1:1)
+  return str.split('').map(e=>object[e]).join(sep)
+}
