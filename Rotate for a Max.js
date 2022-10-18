33009a3efefb207e5522a70f198a6b7b5f7bc280
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-10-17 20:16:45 -0400

    Create Rotate for a Max.js

---
+++ Rotate for a Max.js
@@ -0,0 +1,17 @@
+Write function max_rot(n) which given a positive integer n returns the maximum number you got doing rotations similar to the above example.
+
+So max_rot (or maxRot or ... depending on the language) is such as:
+
+max_rot(56789) should return 68957
+
+max_rot(38458215) should return 85821534
+
+function maxRot(n) {
+    let str = n.toString();
+    let arr = [str];
+    for (let i=0;i<=str.length-1 ;i++){
+        str = str.slice(0,i)+str.slice(i+1)+str[i];
+        arr.push(str.split().join());
+    }
+    return Math.max.apply(null, arr);
+}
