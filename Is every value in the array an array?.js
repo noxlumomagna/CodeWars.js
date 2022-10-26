75a12bbffced1f69be4082aeeaf9713c590f8f1e
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-10-26 09:05:40 +0100

    Create Is every value in the array an array?.js

---
+++ Is every value in the array an array?.js
@@ -0,0 +1,7 @@
+Is every value in the array an array?
+
+This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+
+function arrCheck(value){
+  return value.every(e=>Array.isArray(e))
+}
