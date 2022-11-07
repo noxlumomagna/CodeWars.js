fabb096659d0919db7e45b77c31b977541bdeb5a
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-11-07 14:44:34 -0500

    Update Fizz Buzz Cuckoo Clock.js

--- Fizz Buzz Cuckoo Clock.js
+++ Fizz Buzz Cuckoo Clock.js
@@ -6,16 +6,17 @@ On the half hour, instead of "Fizz Buzz", the clock door will open, and the cuck
 With minutes that are not evenly divisible by either three or five, at first you had intended to have the clock just say the numbers ala Fizz Buzz, but then you decided at least for version 1.0 to just have the clock make a quiet, subtle "tick" sound for a little more clock nature and a little less noise.
 Your input will be a string containing hour and minute values in 24-hour time, separated by a colon, and with leading zeros. For example, 1:34 pm would be "13:34".
 
-function fizzBuzzCuckooClock(time){
-  time = time.split(':').map(e=>parseInt(e))
-  let hours = time[0]
-  let mins = time[1]
+function fizzBuzzCuckooClock(time) {
+  let numberTime = time.split(':').map(e=>parseInt(e))
+  let hours = numberTime[0]
+  let mins = numberTime[1]
   
-  hours = hours === 0 ? 12: hours > 12 ? hours - 12 : hours
-  if(mins===0) return ('Cuckoo '.repeat(hours).trim())
+  hours = hours === 0 ? 12 : hours > 12 ? hours - 12 : hours
+  
+  if(mins === 0) return ('Cuckoo '.repeat(hours).trim())
   if(mins === 30) return ('Cuckoo')
-  if(mins % 15 === 0) return 'Fizz Buzz'
+  if(mins % 15 === 0) return "Fizz Buzz"
   if(mins % 3 === 0) return "Fizz"
-  if(mins % 5 === 0) return 'Buzz'
+  if(mins % 5 === 0) return "Buzz"
   return 'tick'
 }
