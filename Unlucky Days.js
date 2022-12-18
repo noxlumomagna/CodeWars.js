177f662b44400819309bd3ef84f4f5776494474e
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-12-18 11:39:25 -0500

    Create Unlucky Days.js

---
+++ Unlucky Days.js
@@ -0,0 +1,23 @@
+Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+
+Find the number of Friday 13th in the given year.
+
+Input: Year in Gregorian calendar as integer.
+
+Output: Number of Black Fridays in the year as an integer.
+
+Examples:
+
+unluckyDays(2015) == 3
+unluckyDays(1986) == 1
+
+function unluckyDays(year){
+  let fridayTheThirteenth = 0
+  for(let month=0;month<12;month++){
+    let date = new Date(year,month,13)
+    if(date.getDay()===5){
+      fridayTheThirteenth++
+    }
+  }
+  return fridayTheThirteenth
+}
