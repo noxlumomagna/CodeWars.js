763afae68e53b59811c25278caa3be0f292f5666
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2023-05-20 05:52:33 -0400

    Update Calculate average.js

--- Calculate average.js
+++ Calculate average.js
@@ -5,3 +5,7 @@ Note: Empty arrays should return 0.
 function find_average(array){
 return array.length == 0 ? 0 : array.reduce((a,b)=>a+b)/array.length;
 }
+
+or
+
+findAverage=array=>array.length === 0 ? 0 : array.reduce((a,b)=>a+b,0)/array.length
