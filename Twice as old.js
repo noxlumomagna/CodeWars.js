3ef3331acdfc26dbffa5565efc1eca051511d94f
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2023-06-24 14:44:20 -0400

    Update Twice as old.js

--- Twice as old.js
+++ Twice as old.js
@@ -4,6 +4,6 @@ current father's age (years)
 current age of his son (years)
 Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
 
-function twiceAsOld(papa, son) {
-return Math.abs(papa -(2*son));
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
